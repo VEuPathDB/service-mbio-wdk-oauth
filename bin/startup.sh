9245e9a009ce4656fca568b82b43bd8400e334f6
Ryan Doherty <tech@conical.org>
2024-07-02 17:32:00 -0400

    bug fixes

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,7 +2,9 @@
 
 mkdir -p /opt/logs/tomcat /opt/logs/nginx \
     && /opt/bin/configureSite.sh /opt/files /var/www/local.microbiomedb.org \
-    && cp /var/www/local.microbiomedb.org/gus_home/config/mbio.xml /opt/apache-tomcat/conf/Catalina/localhost \
+    && /opt/bin/configureOauth.sh \
     && /opt/apache-tomcat/bin/startup.sh \
+    && bash -c "sleep 10" \
+    && cp /var/www/local.microbiomedb.org/gus_home/config/mbio.xml /opt/apache-tomcat/conf/Catalina/localhost \
     && nginx \
     && exec /bin/bash -c "trap : TERM INT; sleep infinity & wait"
