34417234c76fc84015cd6485ac9558dfe2994795
Ryan Doherty <tech@conical.org>
2024-07-11 06:54:52 -0400

    Add script to build out mounted directory structure

---
+++ bin/buildMountedDirs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+################################################################################
+##
+## buildMountedDirs.sh
+##
+## This script should be run on machines which will host the Dockerized
+## WDK/OAuth/EDA service stack.  The directories created are the default
+## locations for mount points needed by the stack and should house
+## additional config files, download files, and logs of the running services.
+##
+################################################################################
+
+mkdir -p /var/www/Common
+
+mkdir Common/apiSiteFilesMirror
+mkdir Common/apiSiteFilesMirror/webServices
+mkdir Common/apiSiteFilesMirror/downloadSite
+
+mkdir Common/secrets
+
+# The following are config files required by WDK/OAuth (see docs)
+#Common/secrets/cacerts
+#Common/secrets/oauth-keys.pkcs12
+#Common/secrets/wdkSecretKey.txt
+
+mkdir Common/logs
+mkdir Common/logs/nginx
+mkdir Common/logs/tomcat
+
+mkdir Common/tmp
+mkdir Common/tmp/wdkStepAnalysisJobs
