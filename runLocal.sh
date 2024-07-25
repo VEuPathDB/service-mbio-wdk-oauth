39fd794e19032ca875a383cfddf6a159373f2035
Ryan Doherty <tech@conical.org>
2024-07-25 10:40:51 -0400

    Final fixes for basic EDA functionality

--- runLocal.sh
+++ runLocal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker compose -p mbio_local -f docker-compose.yml -f docker-compose.ssh.yml -f docker-compose.dev.yml down -v
+docker compose -p mbio_local -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.ssh.yml down -v
 
-docker compose -p mbio_local -f docker-compose.yml -f docker-compose.ssh.yml -f docker-compose.dev.yml up
+docker compose -p mbio_local -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.ssh.yml up --remove-orphans
