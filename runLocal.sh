ed8f17e046960622bc87490f728f8a15c333cdf3
Ryan Doherty <tech@conical.org>
2024-08-05 10:41:01 -0400

    Reorganize local runtime files a bit

--- runLocal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker compose -p mbio_local -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.ssh.yml down -v
-
-docker compose -p mbio_local -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.ssh.yml up --remove-orphans
